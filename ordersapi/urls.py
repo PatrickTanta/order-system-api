0fa1defcd11e1b1758116cfafed3bc6a4d106be2
PatrickTanta <patrick22karusso97@gmail.com>
2022-05-17 23:34:09 -0500

    feat: adding url for auth me endpint

--- ordersapi/urls.py
+++ ordersapi/urls.py
@@ -7,6 +7,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from users.api.router import router_user
 
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Snippets API",
